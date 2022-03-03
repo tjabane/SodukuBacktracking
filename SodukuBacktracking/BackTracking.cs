0671048aa4c0708706aee4b1256abb1b9915284c
Tshebeletso <t.tjabane@gmail.com>
2022-03-03 10:13:49 +0200

    Added backtracking steps

--- SodukuBacktracking/BackTracking.cs
+++ SodukuBacktracking/BackTracking.cs
@@ -17,7 +17,7 @@ namespace SodukuBacktracking
             nodes = new List<Tuple<int,int,int>>();
         }
 
-        public int[,] GetSolution(int[,] grid, List<Tuple<int, int, int>> nodes, int row, int colomn, bool backTrack)
+        public int[,] GetSolution(int[,] grid, List<(int, int, int)> nodes, int row, int colomn, bool backTrack)
         {
             if (grid[row, colomn] == 0)
             {
@@ -26,34 +26,66 @@ namespace SodukuBacktracking
                 int newValue = GetValidNumber(grid, row, colomn,0);
                 if (newValue != -1)
                 {
-                    (int,int,int) node = (row, colomn, newValue);
+                    (int, int, int) node = (row, colomn, newValue);
                     nodes.Add(node);
-                    return GetSolution()
+                    var nextNode = GetNextNode(row, colomn);
+                    return GetSolution(grid, nodes, nextNode.Item1, nextNode.Item2, false);
                 }
-                    
+                else {
+                    var lastNode = nodes.LastOrDefault();
+                    return GetSolution(grid, nodes, lastNode.Item1, lastNode.Item2, true);
+                }
             }
             else if (grid[row, colomn] != 0 && HasBeenExplored(nodes, row, colomn))
             {
                 // increment till a valid number and go forward 
                 // else set to zero, remove from stack and back trac
+                var currentNode = nodes.LastOrDefault();
+                int newValue = GetValidNumber(grid, row, colomn, currentNode.Item3);
+                if (newValue != -1)
+                {
+                    (int, int, int) node = (row, colomn, newValue);
+                    nodes.Add(node);
+                    var nextNode = GetNextNode(row, colomn);
+                    return GetSolution(grid, nodes, nextNode.Item1, nextNode.Item2, false);
+                }
+                else 
+                {
+                    grid[row, colomn] = 0;
+                    nodes.RemoveAt(nodes.Count - 1);
+                    var lastNode = nodes.LastOrDefault();
+                    return GetSolution(grid, nodes, lastNode.Item1, lastNode.Item2, true);
+                }
             }
-            else if (backTrack)
+            else if (grid[row, colomn] != 0 && !HasBeenExplored(nodes, row, colomn))
             {
                 // skip this node to the previous one
+                var nextNode = GetNextNode(row, colomn);
+                return GetSolution(grid, nodes, nextNode.Item1, nextNode.Item2, false);
             }
             else 
             {
                 // skip for the next node
+                return grid;
             }
         }
-
         private int GetValidNumber(int[,] grid, int row, int colomn, int currentValue = 0)
         {
             if (currentValue == 0) return GetValidNumber(grid, row, colomn, currentValue++);
-            else if (currentValue < 9) return -1;
-             
+            else if (currentValue > 9) return -1;
+
             if (IsValidEntry(grid, row, colomn, currentValue)) return currentValue;
             else return GetValidNumber(grid, row, currentValue);
+        }
+
+        private (int, int) GetNextNode(int currentRow, int currentColomn)
+        {
+            if (currentColomn < 8)
+                return (currentRow, currentColomn++);
+            else if(currentColomn == 8 && currentRow < 8)
+                return (currentRow++, 0);
+            else 
+                return (0, 0);
         }
 
         private bool IsValidEntry(int[,] grid, int row, int colomn, int currentValue)
@@ -61,7 +93,7 @@ namespace SodukuBacktracking
             throw new NotImplementedException();
         }
 
-        private bool HasBeenExplored(List<Tuple<int, int, int>> nodes, int row, int colomn)
+        private bool HasBeenExplored(List<(int, int, int)> nodes, int row, int colomn)
         {
             return nodes.Any(node => node.Item1 == row && node.Item2 == colomn);
         }
