9e5567d719658208bf9297dc635e48713dfad6b6
Tshebeletso <t.tjabane@gmail.com>
2022-03-06 22:12:22 +0200

    Updated Soduku Rules

--- SodukuBacktracking/SodukuRules.cs
+++ SodukuBacktracking/SodukuRules.cs
@@ -1,32 +1,68 @@
 ﻿using SodukuBacktracking.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SodukuBacktracking
 {
     public class SodukuRules
     {
-        public static bool IsRowValid(int[,] grid, int row)
+        public static bool IsValidEntry(int[,] grid, int row, int colomn, int number)
+        {
+            return IsValueValidForRow(grid, row, number) && IsValueValidForColomn(grid, colomn, number) 
+                                                         && IsValueValidForSubGrid(grid, row, colomn, number);
+        }
+
+        private static bool IsValueValidForRow(int[,] grid, int row, int number)
+        {
+            return !grid.GetRow(row).Any(num => num == number);
+        }
+        private static bool IsValueValidForColomn(int[,] grid, int colomn, int number)
+        {
+            return !grid.GetColomn(colomn).Any(num => num == number);
+        }
+
+        private static bool IsValueValidForSubGrid(int[,] grid, int row, int colomn, int number)
+        {
+            var subgrid = GetSubGrid(grid, row, colomn);
+            return !subgrid.Any(num => num == number);
+        }
+
+        public static bool IsSodukuSolved(int[,] grid)
+        {
+            var rowColCheck = Enumerable.Range(0, 9).All(n => IsRowValid(grid, n) && IsColomnValid(grid, n));
+            var subGridCheck = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (!Is3X3GridValid(grid, i * 3, j * 3))
+                    {
+                        subGridCheck = false;
+                        break;
+                    }
+                }
+            }
+            return rowColCheck && subGridCheck;
+        }
+
+        private static bool IsRowValid(int[,] grid, int row)
         {
             var nonZero = grid.GetRow(row).Where(num => num != 0);
             return HasDuplicates(nonZero);
         }
 
-        public static bool IsColomnValid(int[,] grid, int colomn)
+        private static bool IsColomnValid(int[,] grid, int colomn)
         {
             var nonZero = grid.GetColomn(colomn).Where(num => num != 0);
             return HasDuplicates(nonZero);
         }
 
-        public static bool Is3X3GridValid(int[,] grid, int row, int colomn)
+        private static bool Is3X3GridValid(int[,] grid, int row, int colomn)
         {
             var subgrid = GetSubGrid(grid, row, colomn);
             var nonZero = subgrid.Where(num => num != 0);
             return HasDuplicates(nonZero);
         }
+
+        private static bool HasDuplicates(IEnumerable<int> numbers) => numbers.Distinct().Count() == numbers.Count();
 
         private static int[] GetSubGrid(int[,] grid, int row, int colomn)
         {
@@ -40,8 +76,6 @@ namespace SodukuBacktracking
             return collector.ToArray();
         }
 
-        private static bool HasDuplicates(IEnumerable<int> numbers) => numbers.Distinct().Count() == numbers.Count();
-
         private static (int,int) GetGridCornerPosition(int row, int colomn)
         {
             int x = GetIndexPosition(row) * 3;
@@ -49,6 +83,7 @@ namespace SodukuBacktracking
             return (x, y);
         }
 
-        private static int GetIndexPosition(int index) => (int)(Math.Floor(index / 3.0));
+        private static int GetIndexPosition(int index) => (int) Math.Floor(index / 3.0);
+
     }
 }
--- SodukuBacktrackingTests/SodukuRulesTests.cs
+++ SodukuBacktrackingTests/SodukuRulesTests.cs
@@ -7,82 +7,66 @@ namespace SodukuBacktrackingTests
     public class SodukuRulesTests
     {
         [Test]
-        public void ShouldReturnTrueWhenNoRepeatingNumbersInRow()
+        public void ShouldReturnTrueWhenNumberDoesntRepeat()
         {
             int row = 0;
-            int[,] grid = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 } };
+            int col = 1;
+            int entry = 1;
+            int[,] soduku =  {{ 3, 0, 6, 5, 0, 8, 4, 0, 0},
+                              { 5, 2, 0, 0, 0, 0, 0, 0, 0},
+                              { 0, 8, 7, 0, 0, 0, 0, 3, 1},
+                              { 0, 0, 3, 0, 1, 0, 0, 8, 0},
+                              { 9, 0, 0, 8, 6, 3, 0, 0, 5},
+                              { 0, 5, 0, 0, 9, 0, 6, 0, 0},
+                              { 1, 3, 0, 0, 0, 0, 2, 5, 0},
+                              { 0, 0, 0, 0, 0, 0, 0, 7, 4},
+                              { 0, 0, 5, 2, 0, 6, 3, 0, 0}};
 
-            var result = SodukuRules.IsRowValid(grid, row);
+            var result = SodukuRules.IsValidEntry(soduku, row, col, entry);
 
             result.ShouldBeTrue();
         }
 
         [Test]
-        public void ShouldReturnFalseWhenRepeatingNumbersInRow()
+        public void ShouldReturnFalseWhenNumberRepeats()
         {
             int row = 0;
-            int[,] grid = { { 3, 0, 3, 3, 0, 8, 4, 0, 0 } };
+            int col = 1;
+            int entry = 3;
+            int[,] soduku =  {{ 3, 0, 6, 5, 0, 8, 4, 0, 0},
+                              { 5, 2, 0, 0, 0, 0, 0, 0, 0},
+                              { 0, 8, 7, 0, 0, 0, 0, 3, 1},
+                              { 0, 0, 3, 0, 1, 0, 0, 8, 0},
+                              { 9, 0, 0, 8, 6, 3, 0, 0, 5},
+                              { 0, 5, 0, 0, 9, 0, 6, 0, 0},
+                              { 1, 3, 0, 0, 0, 0, 2, 5, 0},
+                              { 0, 0, 0, 0, 0, 0, 0, 7, 4},
+                              { 0, 0, 5, 2, 0, 6, 3, 0, 0}};
 
-            var result = SodukuRules.IsRowValid(grid, row);
+            var result = SodukuRules.IsValidEntry(soduku, row, col, entry);
 
             result.ShouldBeFalse();
         }
 
-
         [Test]
-        public void ShouldReturnTrueWhenNoRepeatingNumbersInColomn()
+        public void ShouldReturnTrueWhenSodukuIsValid()
         {
-            int colomn = 0;
-            int[,] grid = { {1, 2, 3 }, { 4, 5, 6 } };
+            int[,] solvedGrid = {{3,1,6,5,7,8,4,9,2},
+                                 {5,2,9,1,3,4,7,6,8},
+                                 {4,8,7,6,2,9,5,3,1},
+                                 {2,6,3,4,1,5,9,8,7},
+                                 {9,7,4,8,6,3,1,2,5},
+                                 {8,5,1,7,9,2,6,4,3},
+                                 {1,3,8,9,4,7,2,5,6},
+                                 {6,9,2,3,5,1,8,7,4},
+                                 {7,4,5,2,8,6,3,1,9}};
 
-            var result = SodukuRules.IsColomnValid(grid, colomn);
+            var result = SodukuRules.IsSodukuSolved(solvedGrid);
 
             result.ShouldBeTrue();
-        }
-
-        [Test]
-        public void ShouldReturnFalseWhenRepeatingNumbersInColomn()
-        {
-            int colomn = 0;
-            int[,] grid = { { 1, 2, 3 }, { 1, 5, 6 } };
-
-            var result = SodukuRules.IsColomnValid(grid, colomn);
-
-            result.ShouldBeFalse();
-        }
-
-        [Test]
-        public void ShouldReturnTrueWhenNoRepeatingNumbersInSubBlock()
-        {
-            int colomn = 1;
-            int row = 6;
-            int[,] grid = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
-                           {5, 2, 0, 0, 0, 0, 0, 0, 0},
-                           {0, 8, 7, 0, 0, 0, 0, 3, 1},
-                           {0, 0, 3, 0, 1, 0, 0, 8, 0},
-                           {9, 0, 0, 8, 6, 3, 0, 0, 5},
-                           {0, 5, 0, 0, 9, 0, 6, 0, 0},
-                           {1, 3, 0, 0, 0, 0, 2, 5, 0},
-                           {0, 0, 0, 0, 0, 0, 0, 7, 4},
-                           {0, 0, 5, 2, 0, 6, 3, 0, 0}};
-
-            var result = SodukuRules.Is3X3GridValid(grid, row, colomn);
-
-            result.ShouldBeTrue();
-        }
-
-        [Test]
-        public void ShouldReturnFalseWhenRepeatingNumbersInSubBlock()
-        {
-            int colomn = 1;
-            int row = 1;
-            int[,] grid = {{3, 3, 6, 5, 0, 8, 4, 0, 0},
-                           {5, 2, 0, 0, 0, 0, 0, 0, 0},
-                           {0, 8, 7, 0, 0, 0, 0, 3, 1}};
-
-            var result = SodukuRules.Is3X3GridValid(grid, row, colomn);
-
-            result.ShouldBeFalse();
         }
     }
 }
+
+
+
