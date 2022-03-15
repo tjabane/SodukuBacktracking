5f32e99dc8857cfd6c55c5c63061c3bf705b877b
Tshebeletso <t.tjabane@gmail.com>
2022-03-15 02:17:32 +0200

    Validing initial grid Validating new initial grid

--- SodukuBacktracking/BackTracking.cs
+++ SodukuBacktracking/BackTracking.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SodukuBacktracking.Exceptions;
+using SodukuBacktracking.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +10,79 @@ namespace SodukuBacktracking
 {
     public class BackTracking
     {
+        private static readonly int MaximumIterations = 10000000;
         public int[,] Solve(int[,] grid)
         {
+            if (SodukuRules.GridValid(grid))
+                throw new InvalidGridException("Soduku contains repeating numbers");
             var nodes = new List<(int, int, int)>();
-            return GetSolution(grid, nodes, 0, 0);
+            return GetLoopMethodSolution(grid, nodes);
         }
 
-        private int[,] GetSolution(int[,] grid, List<(int, int, int)> nodes, int row, int colomn)
+        private static int[,] GetLoopMethodSolution(int[,] grid, List<(int, int, int)> nodes)
+        {
+            (int, int) positionNode = (0, 0);
+            for (int i = 0; i < MaximumIterations; i++)
+            {
+                var row = positionNode.Item1;
+                var colomn = positionNode.Item2;
+                if (SodukuRules.IsSodukuSolved(grid))
+                    return grid;
+                
+
+                if (grid[row, colomn] == 0)
+                {
+                    int newValue = GetValidNumber(grid, row, colomn, 0);
+                    if (newValue != -1)
+                    {
+                        grid[row, colomn] = newValue;
+                        (int, int, int) node = (row, colomn, newValue);
+                        nodes.Add(node);
+                        positionNode = GetNextNode(row, colomn);
+                    }
+                    else
+                    {
+                        var lastNode = nodes.LastOrDefault();
+                        positionNode = (lastNode.Item1, lastNode.Item2);
+                    }
+                }
+                else if (grid[row, colomn] != 0 && HasBeenExplored(nodes, row, colomn))
+                {
+                    var currentNode = nodes.LastOrDefault();
+                    int newValue = GetValidNumber(grid, row, colomn, currentNode.Item3);
+                    if (newValue != -1)
+                    {
+                        grid[row, colomn] = newValue;
+                        nodes.Remove(currentNode);
+                        (int, int, int) node = (row, colomn, newValue);
+                        nodes.Add(node);
+                        positionNode = GetNextNode(row, colomn);
+                    }
+                    else
+                    {
+                        grid[row, colomn] = 0;
+                        nodes.Remove(nodes.LastOrDefault());
+                        if (nodes.Count == 0)
+                            throw new UnSolvableException("No solution exists");
+                        var lastNode = nodes.LastOrDefault();
+                        positionNode = (lastNode.Item1, lastNode.Item2);
+                    }
+                }
+                else if (grid[row, colomn] != 0 && !HasBeenExplored(nodes, row, colomn))
+                {
+                    positionNode = GetNextNode(row, colomn);
+                }
+            }
+            throw new UnSolvableException("Maximun iterations exceeded");
+        }
+
+        private int[,] GetSolution(int[,] grid, List<(int, int, int)> nodes, int row=0, int colomn=0)
         {
             if (SodukuRules.IsSodukuSolved(grid))
                 return grid;
-
             if (grid[row, colomn] == 0)
             {
-                // increment till a valid number and go forward 
-                // else back track
-                int newValue = GetValidNumber(grid, row, colomn,0);
+                int newValue = GetValidNumber(grid, row, colomn, 0);
                 if (newValue != -1)
                 {
                     grid[row, colomn] = newValue;
@@ -32,15 +91,14 @@ namespace SodukuBacktracking
                     var nextNode = GetNextNode(row, colomn);
                     return GetSolution(grid, nodes, nextNode.Item1, nextNode.Item2);
                 }
-                else {
+                else
+                {
                     var lastNode = nodes.LastOrDefault();
                     return GetSolution(grid, nodes, lastNode.Item1, lastNode.Item2);
                 }
             }
             else if (grid[row, colomn] != 0 && HasBeenExplored(nodes, row, colomn))
             {
-                // increment till a valid number and go forward 
-                // else set to zero, remove from stack and back trac
                 var currentNode = nodes.LastOrDefault();
                 int newValue = GetValidNumber(grid, row, colomn, currentNode.Item3);
                 if (newValue != -1)
@@ -52,8 +110,9 @@ namespace SodukuBacktracking
                     var nextNode = GetNextNode(row, colomn);
                     return GetSolution(grid, nodes, nextNode.Item1, nextNode.Item2);
                 }
-                else 
+                else
                 {
+
                     grid[row, colomn] = 0;
                     nodes.Remove(nodes.LastOrDefault());
                     var lastNode = nodes.LastOrDefault();
@@ -65,18 +124,20 @@ namespace SodukuBacktracking
                 var nextNode = GetNextNode(row, colomn);
                 return GetSolution(grid, nodes, nextNode.Item1, nextNode.Item2);
             }
-            else if(row == 8 && colomn == 8)
+            else if (row == 8 && colomn == 8)
             {
                 return grid;
             }
             else
                 return grid;
+
         }
-        private int GetValidNumber(int[,] grid, int row, int colomn, int currentValue = 0)
+        private static int GetValidNumber(int[,] grid, int row, int colomn, int currentValue = 0)
         {
             if (currentValue == 0) 
                 return GetValidNumber(grid, row, colomn, ++currentValue);
-            else if (currentValue > 9) return -1;
+            else if (currentValue > 9) 
+                return -1;
 
             if (SodukuRules.IsValidEntry(grid, row, colomn, currentValue)) return currentValue;
             else return GetValidNumber(grid, row, colomn, ++currentValue);
--- SodukuBacktrackingTests/BackTrackingTests.cs
+++ SodukuBacktrackingTests/BackTrackingTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Shouldly;
 using SodukuBacktracking;
+using SodukuBacktracking.Exceptions;
 
 namespace SodukuBacktrackingTests
 {
@@ -23,6 +24,42 @@ namespace SodukuBacktrackingTests
             isSolved.ShouldBeTrue();
         }
 
+        [Test]
+        public void ShouldThrowErrorWhenSodukuUnSolvable()
+        {
+            var problem = GetUnSolvableSoduku();
+            var backTracking = new BackTracking();
+
+            var exception = Assert.Throws<UnSolvableException>(() => backTracking.Solve(problem));
+            exception.Message.ShouldBe("Maximun iterations exceeded");
+        }
+
+        [Test]
+        public void ShouldReturnFalseWhenSodukuInvalid()
+        {
+            var soduku = GetInvalidSoduku();
+            var backTracking = new BackTracking();
+
+            var exception = Assert.Throws<InvalidGridException>(() => backTracking.Solve(soduku));
+            exception.Message.ShouldBe("Soduku contains repeating numbers");
+        }
+
+        private static int[,] GetInvalidSoduku()
+        {
+            int[,] grid =   {{ 3, 3, 6, 5, 0, 8, 4, 0, 0},
+                             { 5, 2, 0, 0, 0, 0, 0, 0, 0},
+                             { 0, 8, 7, 0, 0, 0, 0, 3, 1},
+                             { 0, 0, 3, 0, 1, 0, 0, 8, 1},
+                             { 9, 0, 0, 8, 6, 3, 0, 0, 5},
+                             { 0, 5, 0, 0, 9, 0, 6, 0, 0},
+                             { 1, 3, 0, 0, 0, 0, 2, 5, 0},
+                             { 0, 0, 0, 0, 0, 0, 0, 7, 4},
+                             { 0, 0, 5, 2, 0, 6, 3, 0, 2}};
+            return grid;
+        }
+
+
+
         private static int[,] GetSolvableSoduku()
         {
             int[,] grid =   {{ 3, 0, 6, 5, 0, 8, 4, 0, 0}, 
@@ -34,6 +71,20 @@ namespace SodukuBacktrackingTests
                              { 1, 3, 0, 0, 0, 0, 2, 5, 0}, 
                              { 0, 0, 0, 0, 0, 0, 0, 7, 4}, 
                              { 0, 0, 5, 2, 0, 6, 3, 0, 0}};
+            return grid;
+        }
+
+        private static int[,] GetUnSolvableSoduku()
+        {
+            int[,] grid =   {{ 2, 0, 0, 9, 0, 0, 0, 0, 0},
+                             { 0, 0, 0, 0, 0, 0, 0, 6, 0},
+                             { 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                             { 5, 0, 2, 6, 0, 0, 4, 0, 7},
+                             { 0, 0, 0, 0, 0, 4, 1, 0, 0},
+                             { 0, 0, 0, 0, 9, 8, 0, 2, 3},
+                             { 0, 0, 0, 0, 0, 3, 0, 8, 0},
+                             { 0, 0, 5, 0, 1, 0, 0, 0, 0},
+                             { 0, 0, 7, 0, 0, 0, 0, 0, 0}};
             return grid;
         }
 
