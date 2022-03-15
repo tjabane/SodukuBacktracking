be40a4c10e4c09d936db5cc3cfe94ad4ec4a2ba0
Tshebeletso <t.tjabane@gmail.com>
2022-03-15 02:04:40 +0200

    Unsolvable exception

---
+++ SodukuBacktracking/Exceptions/UnSolvableException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukuBacktracking.Exceptions
+{
+    public class UnSolvableException : Exception
+    {
+        public UnSolvableException() { }
+        public UnSolvableException(string message) : base(message) { }
+    }
+}
