e1e0ac48533d9b855b721bce204f142d56b719d9
Tshebeletso <t.tjabane@gmail.com>
2022-03-21 00:00:17 +0200

    New Exception

---
+++ SodukuBacktracking/Exceptions/InvalidSodukuValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukuBacktracking.Exceptions
+{
+    public class InvalidSodukuValue: Exception
+    {
+        public InvalidSodukuValue() : base() { }
+
+        public InvalidSodukuValue(string message) : base(message) { }
+    }
+}
