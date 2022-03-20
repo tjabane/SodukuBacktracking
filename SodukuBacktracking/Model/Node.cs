fe1733841f798050beb9157339160a9de4b39263
Tshebeletso <t.tjabane@gmail.com>
2022-03-21 00:21:45 +0200

    Remove tuples for new node object

---
+++ SodukuBacktracking/Model/Node.cs
@@ -0,0 +1,51 @@
+﻿using SodukuBacktracking.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodukuBacktracking.Model
+{
+    public class Node
+    {
+        private Node(int row, int colomn, int value)
+        {
+            Row = row;
+            Colomn = colomn;
+            Value = value;
+        }
+
+        public static Node CreateNode(int row, int colomn, int value)
+        {
+            if(IsInvalidSodukuIndex(row))
+                throw new InvalidSodukuValue("Invalid Row index");
+            if(IsInvalidSodukuIndex(colomn))
+                throw new InvalidSodukuValue("Invalid Colomn index");
+            if(IsInvalidSodukuValue(value))
+                throw new InvalidSodukuValue("Invalid Value");
+            return new Node(row, colomn, value);
+        }
+
+        private static bool IsInvalidSodukuIndex(int index)
+        {
+            return (index < 0 || index > 8);
+        }
+
+        private static bool IsInvalidSodukuValue(int value)
+        {
+            return (value < 1 || value > 9);
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(Row, Colomn);
+        }
+
+        public int Row { get; }
+
+        public int Colomn { get; }
+
+        public int Value { get;}
+    }
+}
---
+++ SodukuBacktrackingTests/Model/NodeTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using SodukuBacktracking.Exceptions;
+using SodukuBacktracking.Model;
+using Shouldly;
+
+namespace SodukuBacktrackingTests.Model
+{
+    public class NodeTests
+    {
+        [Test]
+        public void ShouldThrowErrorWhenRowOutOfRange()
+        {
+            int row = 10;
+            int colomn = 5;
+            int value = 6;
+
+            var exception = Assert.Throws<InvalidSodukuValue>(() => Node.CreateNode(row, colomn, value));
+            exception.Message.ShouldBe("Invalid Row index");
+        }
+
+        [Test]
+        public void ShouldThrowErrorWhenColomnOutOfRange()
+        {
+            int row = 5;
+            int colomn = 50;
+            int value = 6;
+
+            var exception = Assert.Throws<InvalidSodukuValue>(() => Node.CreateNode(row, colomn, value));
+            exception.Message.ShouldBe("Invalid Colomn index");
+        }
+
+        [Test]
+        public void ShouldThrowErrorWhenValueOutOfRange()
+        {
+            int row = 5;
+            int colomn = 5;
+            int value = 600;
+
+            var exception = Assert.Throws<InvalidSodukuValue>(() => Node.CreateNode(row, colomn, value));
+            exception.Message.ShouldBe("Invalid Value");
+        }
+
+
+        [Test]
+        public void ShouldCreateNodeWhenValuesInRange()
+        {
+            int row = 5;
+            int colomn = 5;
+            int value = 6;
+
+            var node = Node.CreateNode(row, colomn, value);
+
+            node.Row.ShouldBe(row);
+            node.Colomn.ShouldBe(colomn);
+            node.Value.ShouldBe(value);
+        }
+
+    }
+}
