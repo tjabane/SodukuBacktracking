c9f5162b6793ce96aa7300352afc25a66d0d4284
Tshebeletso <t.tjabane@gmail.com>
2022-03-21 00:48:21 +0200

    Position object

---
+++ SodukuBacktracking/Model/Position.cs
@@ -0,0 +1,31 @@
+﻿using SodukuBacktracking.Exceptions;
+
+namespace SodukuBacktracking.Model
+{
+    public class Position
+    {
+        private Position(int row, int colomn)
+        {
+            Row = row;
+            Colomn = colomn;
+        }
+
+        public static Position GetPosition(int row, int colomn)
+        {
+            if (IsInvalidSodukuIndex(row))
+                throw new InvalidSodukuValue("Invalid Row index");
+            if (IsInvalidSodukuIndex(colomn))
+                throw new InvalidSodukuValue("Invalid Colomn index");
+            return new Position(row, colomn);
+        }
+
+        private static bool IsInvalidSodukuIndex(int index)
+        {
+            return (index < 0 || index > 8);
+        }
+
+        public int Row { get; }
+
+        public int Colomn { get; }
+    }
+}
---
+++ SodukuBacktrackingTests/Model/PositionTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using SodukuBacktracking.Exceptions;
+using SodukuBacktracking.Model;
+
+namespace SodukuBacktrackingTests.Model
+{
+    public class PositionTests
+    {
+        [Test]
+        public void ShouldThrowErrorWhenRowOutOfRange()
+        {
+            int invalidRow = 10;
+            int colomn = 5;
+
+            var exception = Assert.Throws<InvalidSodukuValue>(() => Position.GetPosition(invalidRow, colomn));
+            exception.Message.ShouldBe("Invalid Row index");
+        }
+
+        [Test]
+        public void ShouldThrowErrorWhenColomnOutOfRange()
+        {
+            int row = 5;
+            int invaliColomn = 50;
+
+            var exception = Assert.Throws<InvalidSodukuValue>(() => Position.GetPosition(row, invaliColomn));
+            exception.Message.ShouldBe("Invalid Colomn index");
+        }
+
+        [Test]
+        public void ShouldReturnPositionWhenValuesInRange()
+        {
+            int row = 5;
+            int colomn = 5;
+
+            var position = Position.GetPosition(row, colomn);
+
+            position.Row.ShouldBe(row);
+            position.Colomn.ShouldBe(colomn);
+        }
+    }
+}
